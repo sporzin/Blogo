e2862ea48c32089077232489907c01bf2421d376
pouyababaie <pouyababaie123@gmail.com>
2022-01-16 17:20:01 +0330

    additional packages added

--- Front/src/landing/public-layout/public-layout.component.ts
+++ Front/src/landing/public-layout/public-layout.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-public-layout',
   templateUrl: './public-layout.component.html',
-  styleUrls: ['./public-layout.component.scss']
+  styleUrls: ['./public-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PublicLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- Front/src/root.module.ts
+++ Front/src/root.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { RootRoutingModule } from './root-routing.module';
 import { RootComponent } from './root.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [RootComponent],
-  imports: [BrowserModule, CommonModule, RootRoutingModule],
+  imports: [BrowserModule, CommonModule, RootRoutingModule, FontAwesomeModule],
   bootstrap: [RootComponent],
 })
 export class RootModule {}
