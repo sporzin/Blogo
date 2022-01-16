ffa54ae9227f50d29c166b3d8ab4b67391997d2a
pouyababaie <pouyababaie123@gmail.com>
2022-01-16 16:50:00 +0330

    added home to landing routing

--- Front/src/landing/landing-routing.module.ts
+++ Front/src/landing/landing-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        pathMatch: 'prefix',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LandingRoutingModule { }
+export class LandingRoutingModule {}
--- Front/src/landing/landing.module.ts
+++ Front/src/landing/landing.module.ts
@@ -5,13 +5,15 @@ import { LandingRoutingModule } from './landing-routing.module';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { PublicHeaderComponent } from './shared/public-header/public-header.component';
 import { PublicFooterComponent } from './shared/public-footer/public-footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     PublicLayoutComponent,
     PublicHeaderComponent,
-    PublicFooterComponent
+    PublicFooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
