a5a5417938562ff965bb2d312e9eacb2faea6f75
pouyababaie <pouyababaie123@gmail.com>
2022-01-18 14:30:41 +0330

    cors policy added

--- Server/main.py
+++ Server/main.py
@@ -5,11 +5,24 @@ from fastapi import FastAPI
 from controllers.posts import PostsRouter
 from controllers.users import UserRouter
 from src.db import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
 
 Blogo = FastAPI()
+
+
+origins = ['*']
+
+Blogo.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 Blogo.include_router(PostsRouter)
 Blogo.include_router(UserRouter)
