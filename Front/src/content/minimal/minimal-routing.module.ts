1606fc1c45571c39e08b4a02b5cf0d3b8fb72e1e
pouyababaie <pouyababaie123@gmail.com>
2022-01-30 00:42:07 +0330

    added minimal module loading

--- Front/src/content/minimal/minimal-routing.module.ts
+++ Front/src/content/minimal/minimal-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MinimalComponent } from './minimal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MinimalComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MinimalRoutingModule { }
+export class MinimalRoutingModule {}
