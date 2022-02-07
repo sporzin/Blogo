2ed13e617fb4d89426157fda1485a0ee1b7846ec
pouyababaie <pouyababaie123@gmail.com>
2022-02-08 01:45:12 +0330

    fixed login nav item view

--- Front/src/landing/shared/public-header/public-header.component.ts
+++ Front/src/landing/shared/public-header/public-header.component.ts
@@ -1,15 +1,25 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/account/shared/services/auth.service';
 
 @Component({
   selector: 'app-public-header',
   templateUrl: './public-header.component.html',
-  styleUrls: ['./public-header.component.scss']
+  styleUrls: ['./public-header.component.scss'],
 })
 export class PublicHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  isLoggedIn: boolean = false;
+  constructor(private authService: AuthService) {
+    this.authService.is_authenticated().subscribe((result) => {
+      this.isLoggedIn = result;
+    });
   }
 
+  ngOnInit(): void {}
+
+  logOut() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    window.location.reload();
+  }
 }
