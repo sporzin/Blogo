71df7a8f72936ed945b444892e0439ca6656dc6a
pouyababaie <pouyababaie123@gmail.com>
2022-02-08 01:44:48 +0330

    removed unneccery modulee

--- Front/src/root-routing.module.ts
+++ Front/src/root-routing.module.ts
@@ -13,11 +13,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./landing/landing.module').then((m) => m.LandingModule),
   },
-  {
-    path: 'contents',
-    loadChildren: () =>
-      import('./content/content.module').then((m) => m.ContentModule),
-  },
+
   {
     path: 'account',
     loadChildren: () =>
