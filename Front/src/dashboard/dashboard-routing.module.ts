faeb0d346c6c96e62feb483714198ede29d4a9d8
pouyababaie <pouyababaie123@gmail.com>
2022-02-08 01:53:54 +0330

    dasboard module improved

--- Front/src/dashboard/dashboard-routing.module.ts
+++ Front/src/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'blog-single',
+        loadChildren: () => import('./posts/blog-single/blog-single.module'),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
