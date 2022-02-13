2d631fe04b449fcbdca7d6fdd548dea3db2dc263
pouyababaie <pouyababaie123@gmail.com>
2022-02-13 18:09:16 +0330

    add CkEditor

--- Front/src/dashboard/dashboard-header/dashboard-header.component.ts
+++ Front/src/dashboard/dashboard-header/dashboard-header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-header',
@@ -6,13 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-header.component.scss'],
 })
 export class DashboardHeaderComponent implements OnInit {
-  constructor() {}
+  @ViewChild('post') post: ElementRef<HTMLLIElement>;
+  @ViewChild('user') user: ElementRef<HTMLLIElement>;
+  constructor(private renderer2: Renderer2) {}
 
   ngOnInit(): void {}
 
-  createPost() {}
-
-  createUser() {}
+  createUser() {
+    this.renderer2.removeClass(this.post.nativeElement, 'active');
+    this.renderer2.addClass(this.user.nativeElement, 'active');
+  }
 
   logout() {}
 }
