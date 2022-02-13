05ecb0c464191a7e9561885903bbf6f3ca293133
pouyababaie <pouyababaie123@gmail.com>
2022-02-13 15:21:03 +0330

    dashboard routing added

--- Front/src/dashboard/dashboard-routing.module.ts
+++ Front/src/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/account/shared/services/auth.guard';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
--- Front/src/root-routing.module.ts
+++ Front/src/root-routing.module.ts
@@ -19,6 +19,12 @@ const routes: Routes = [
     loadChildren: () =>
       import('./account/account.module').then((m) => m.AccountModule),
   },
+
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
   {
     path: '**',
     component: NotFoundComponent,
